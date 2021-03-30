4b65a428e8c04d1d6078ae89239629c5026e317b
Enda Phelan <ephelan@redhat.com>
2021-03-30 14:13:58 +0100

    fix: do not install to /usr

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,70 +2,79 @@
 
 { # this ensures the entire script is downloaded #
 
-BINARY_DEST="${1:-/usr/local/bin}"
-BINARY_NAME="rhoas"
-SRC_ORG="bf3fc6c"
-SRC_REPO="cli"
-OS_TYPE="linux"
-OS_LONG_BIT="64"
-API_BASE_URL="https://api.github.com"
-API_RELEASE_URL="${API_BASE_URL}/repos/${SRC_ORG}/${SRC_REPO}/releases/latest"
-DOWNLOAD_DIR="/tmp"
+  BINARY_NAME="rhoas"
+  SRC_ORG="bf3fc6c"
+  SRC_REPO="cli"
+  OS_TYPE="linux"
+  OS_LONG_BIT="64"
+  RELEASE_TAG="${1:-latest}"
+  API_BASE_URL="https://api.github.com"
+  API_RELEASES_BASE_URL="${API_BASE_URL}/repos/${SRC_ORG}/${SRC_REPO}/releases"
+  DOWNLOAD_DIR="/tmp"
+  BINARY_DEST="$HOME/.local/bin"
 
-has_in_path() {
-  type "$1" > /dev/null 2>&1
-}
+  API_RELEASE_URL="$API_RELEASES_BASE_URL/latest"
+  if [ "$RELEASE_TAG" != "latest" ]; then
+    API_RELEASE_URL="$API_RELEASES_BASE_URL/tags/$RELEASE_TAG"
+  fi
 
-source_file() {
-  # shellcheck disable=SC1090
-  source "$1" 2> /dev/null
-}
+  has_in_path() {
+    type "$1" >/dev/null 2>&1
+  }
 
-# get OS type
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  OS_TYPE="macOS"
-fi
+  source_file() {
+    # shellcheck disable=SC1090
+    source "$1" 2>/dev/null
+  }
 
-if ! has_in_path "curl"; then
-  echo "curl is required to download this binary"
-  exit 1
-fi
+  # update config based on OS type
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    OS_TYPE="macOS"
+    BINARY_DEST="$HOME/bin"
+  fi
 
-if [ ! -d "$BINARY_DEST" ]; then
-  mkdir "$BINARY_DEST"
-fi
+  if ! has_in_path "curl"; then
+    echo "curl is required to download this binary"
+    exit 1
+  fi
 
-LATEST_TAG=$(curl -s "${API_RELEASE_URL}" \
-| grep "tag_name.*" \
-| cut -d '"' -f 4)
+  if [ ! -d "$BINARY_DEST" ]; then
+    mkdir -p "$BINARY_DEST"
+  fi
 
-ASSET_NAME="${BINARY_NAME}_${LATEST_TAG}_${OS_TYPE}_amd${OS_LONG_BIT}"
-ASSET_NAME_COMPRESSED="${ASSET_NAME}.tar.gz"
+  DOWNLOAD_TAG=$(curl -s "${API_RELEASE_URL}" |
+    grep "tag_name.*" |
+    cut -d '"' -f 4)
 
-DOWNLOAD_URL=$(curl -s "${API_RELEASE_URL}" \
-| grep "browser.download_url.*${ASSET_NAME_COMPRESSED}" \
-| cut -d '"' -f 4)
+  if [ -z "$DOWNLOAD_TAG" ]; then
+    echo "Release tag $RELEASE_TAG not found"
+    exit 1
+  fi
 
-cd "$DOWNLOAD_DIR" || exit
+  ASSET_NAME="${BINARY_NAME}_${DOWNLOAD_TAG}_${OS_TYPE}_amd${OS_LONG_BIT}"
+  ASSET_NAME_COMPRESSED="${ASSET_NAME}.tar.gz"
 
-# wget is faster, use it to download the release if available
-if has_in_path "wget"; then
-  echo "Downloading $BINARY_NAME v${LATEST_TAG}"
-  wget -q "$DOWNLOAD_URL"
-else
-  echo "Downloading $BINARY_NAME v${LATEST_TAG}"
-  curl -sL "$DOWNLOAD_URL" --output "${ASSET_NAME_COMPRESSED}"
-fi
+  DOWNLOAD_URL=$(curl -s "${API_RELEASE_URL}" |
+    grep "browser.download_url.*${ASSET_NAME_COMPRESSED}" |
+    cut -d '"' -f 4)
 
-  echo "$BINARY_NAME v${LATEST_TAG} downloaded"
+  cd "$DOWNLOAD_DIR" || exit
 
+  # wget is faster, use it to download the release if available
+  if has_in_path "wget"; then
+    echo "Downloading $BINARY_NAME v${DOWNLOAD_TAG}"
+    wget -q "$DOWNLOAD_URL"
+  else
+    echo "Downloading $BINARY_NAME v${DOWNLOAD_TAG}"
+    curl -sL "$DOWNLOAD_URL" --output "${ASSET_NAME_COMPRESSED}"
+  fi
 
-# unpack and place the binary in the users PATH
-tar xvf "$ASSET_NAME_COMPRESSED"
-rm -rf "${ASSET_NAME_COMPRESSED}"
-cp "${ASSET_NAME}/bin/${BINARY_NAME}" "${BINARY_DEST}/${BINARY_NAME}"
+  echo "$BINARY_NAME v${DOWNLOAD_TAG} downloaded"
 
-echo "$BINARY_DEST"
+  # unpack and place the binary in the users PATH
+  tar xf "$ASSET_NAME_COMPRESSED"
+  cp "${ASSET_NAME}/bin/${BINARY_NAME}" "${BINARY_DEST}/${BINARY_NAME}"
 
-echo "$BINARY_NAME v$LATEST_TAG"
+  echo "rhoas has been installed succesfully to $BINARY_DEST"
+  echo "Please ensure that $BINARY_DEST is on your PATH"
 } # this ensures the entire script is downloaded #
